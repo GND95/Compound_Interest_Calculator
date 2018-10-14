24115741ccb896faebaa112b9992bbb20e8c29eb
Garrett <Garrettdeblois13@yahoo.com>
2018-10-13 20:47:18 -0500

    Initial push.

--- Compound Interest Calculator/Program.cs
+++ Compound Interest Calculator/Program.cs
@@ -10,6 +10,36 @@ namespace Compound_Interest_Calculator
     {
         static void Main(string[] args)
         {
+            double interestRate, numberOfYears, total, interestGains;
+            double Calculate(double Starting, double IR)
+            {
+                interestGains = Starting * (IR / 100);
+                total = interestGains + Starting;
+
+                return (Math.Round(total, 2));
+            }
+
+            try
+            {
+                Console.WriteLine("What is your starting value?");
+                total = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is your interest rate (%) per year?");
+                interestRate = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("How many years are you keeping this money in here?");
+                numberOfYears = Convert.ToDouble(Console.ReadLine());
+
+                for (int i = 0; i < numberOfYears; i++)
+                {
+                    Console.WriteLine("Your total for year " + (i + 1) + " is $" + Calculate(total, interestRate) + " and you've gained $" + Math.Round(interestGains, 2) + " in interest this year.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter numbers only. Press enter to restart.");
+            }
+            Console.ReadLine();
+            Console.Clear();
+            Main(args);
         }
     }
 }
